3315634511bfcda301cf18e67415fe394f7b6a1f
davidquesadaheredia <deividqh@gmail.com>
2026-06-13 12:08:20 +0200

    salvando main

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # XindeX
 Indice Python para Terminal
+
+quiero que el indice se genere  con streamlit (ejecuta_st.py y ejecutado_st.py)
+y se recuperen los datos en un diccionario. y cuando cierre streamlit vuelva  al 
+programa principal y pueda ejecutar el indice creado en streamlit.
+veo estas opciones para hacer este proceso. quiero que las valides para ver si 
+es viable lo que sugiero.
+no quiero codigo aun, solo quiero analisis y orientación, eso si, analiza el codigo.
+el streamlit que quiero ejecutar para configurar el indice es tal como ./identa/identa6.html 
+y quiero que se ejecute y retorne comno ./ejecuta_st/ejecuta_st.py y ./ejecuta_st/ejecutado_st.py
+
+las opciones que se me han ocurrido y que quiero que analices serían:
+
+# opcion 1
+Se abre Over_Main, este abre como subprocess streamlit que genera un diccionario qeu cuando se 
+pulsa el boton se envía a Over_Main y se muestra el resultado con mystica. 
+a partir de ahí se sigue en terminal.
+
+■ problema: como se ejecutan las funciones?
+■ problema: usuario CIERRA STREAMLIT A LAS BRAVAS
+
+# opcion 2
+Se abre Over_Main
+if json and b_config=True ??    
+    se lee json ► streamlit 
+    if boton_st:
+        recibe json + lee json
+        mystica +     se sigue en terminal.
+    else:   
+        (CIERRA STREAMLIT A LAS BRAVAS)
+
+    mystyca +    se sigue en terminal.
+
+elif json and b_config=False:
+    mystica +     se sigue en terminal.
+elif not json and b_config=True:
+    streamlit:
+    if boton_st:
+        recibe json + lee json
+        mystica +     se sigue en terminal.
+    else:   
+        (CIERRA STREAMLIT A LAS BRAVAS)
+elif not json and b_config=False:
+
+# opcion 3
+Abre streamlit y ejecuta las funciones del terminal en web.
+Tengo que crear un mapa(diccionario nombre_funcion:funcion) en el programa principal
+
+
--- over_main.py
+++ over_main.py
@@ -4,73 +4,63 @@ from XindeX.classXindeX import Over_Main       # ■ PADRE DE XINDEX CON ■ COL
 from colorama import Fore, Style, init  # ■ COLORAMA PARA COLORES EN TERMINAL....por si se quiere usar colores para 'ayudas'
 # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 import os               # SISTEMA OPERATIVO(PARA LIMPIAR LA TERMINAL)
-# ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 import multiprocessing  # PARA PROCESOS EN BACKGROUND Y DEMONIOS
-# ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+# ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 import funciones_over_main as cmd   # ■ (Opt)FUNCIONES ASOCIADAS A LOS ITEMS DE LOS MENUS, SE DEFINEN EN ESTE ARCHIVO PARA SIMPLIFICAR LA LECTURA DE ESTE ARCHIVO PRINCIPAL Y PARA QUE LAS FUNCIONES PUEDAN ACCEDER AL OBJETO MENU PRINC
-
 # ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 # ████████████████████████████████████████████ XINDEX ███████████████████████████████████████████████
-# DEF: CREA UN INDICE MULTINIVEL CON GENETICA QUE EJECUTA LAS FUNCIONES ASOCIADAS A CADA MENU
+# DEF: CREA UN INDICE MULTINIVEL QUE EJECUTA LAS FUNCIONES ASOCIADAS A CADA MENU
 # ████████████████████████████████████████████ XINDEX ███████████████████████████████████████████████
 
 # INSTANCIO EL OBJETO ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 The_X_Men = Over_Main(tipo_index='a', b_mode_all=True, b_loop=True )
-
-
-# ( Opt ) Tiene que haber un The_X_Men declarado global en funciones_over_ma
+# ( Opt ) Tiene que haber un The_X_Men declarado global en funciones_over_main
 cmd.The_X_Men = The_X_Men
-
 def main():
     # ■- CREO LOS MENUS Y SUS FUNCIONES ASOCIADAS ▄▄▄▄▄▄▄▄▄▄▄▄
     Menu1 = The_X_Men.addX(titulo='Menu1', padre=None , ipadre=None, 
                     lst_items = [   ("Info XindeX" , None), 
                                     ("Estilos" , None),
                                     ("Procesos" , None), 
-                                    ("From-To" ,  cmd.from_to) ] )
-    
-    The_X_Men.addX( titulo='sub-Xindex' , padre='Menu1'   , ipadre="Info XindeX", 
+                                    ("From - To" ,  cmd.from_to) ,
+                                    ] )
+    # ■                                    
+    The_X_Men.addX( titulo='sub-Xindex' , padre='Menu1'  , ipadre="Info XindeX" , 
                     lst_items = [   ("Sobre XindeX" , None) , 
-                                    ("Parametros Mystyca", None ), 
-                                    ("Ejemplos Uso" , None ) ] )    
-    
-    The_X_Men.addX( titulo='sub-Estilos', padre='Menu1'   , ipadre='Estilos'    , 
+                                    ("Parametros Mystyca", None ) , 
+                                    ("Ejemplos Uso" , None ) 
+                                    ] )    
+    The_X_Men.addX( titulo='sub-Estilos', padre='Menu1'  , ipadre='Estilos' , 
                     lst_items = [   ("Ver Estilos", None) , 
-                                    ("Version Web" ,  cmd.version_web) ] )    
-    
-    The_X_Men.addX( titulo='sub-Procesos', padre='Menu1'  , ipadre=2   , 
+                                    ("Version Web" ,  cmd.version_web) ,
+                                    ] )    
+    The_X_Men.addX( titulo='sub-Procesos', padre='Menu1' , ipadre=2 , 
                     lst_items = [   ("Listar Proceso",  cmd.listar_procesos) , 
-                                    ("Lanzar Proceso" ,  cmd.lanzar_proceso), 
+                                    ("Lanzar Proceso" ,  cmd.lanzar_proceso) , 
                                     ("Detener Proceso" , None) , 
-                                    ('OS procesos',  cmd.subprocess_os)] )    
-    
-    The_X_Men.addX( titulo='sub-Aux', padre='sub-Estilos' , ipadre='Ver Estilos', 
+                                    ('OS process',  cmd.subprocess_os), 
+                                    ] )    
+    # ■ ■                                    
+    The_X_Men.addX( titulo='sub-Aux', padre='sub-Estilos' , ipadre='Ver Estilos' , 
                     lst_items = [   ("Switch b_mode_all",  cmd.set_style), 
                                     ("Cambiar Estilo Marco" ,  cmd.cambiar_estilo_marco), 
-                                    ("aux 3" , None) ] )    
+                                    ("aux 3" , None) ,
+                                    ] )    
     
-    # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ FORMA LARGA Y MAS ANTIGUA.
     # ■ AÑADE Y CONFIGURA - es el camino largo... o por partes.
-    # The_X_Men.addX(titulo='subXindex', lst_items=[ ("Info Inicial" , None) ,  ("Configuracion XindeX", None ), ("Explicacion Parametros", parametros ), ("Ejemplos Uso" , None ) ])
-    # The_X_Men.config(titulo='subXindex' , suPadre='Menu1' , indexInPadre='Info XindeX' )    
+    # ■ ■ ■ FORMA LARGA Y MAS ANTIGUA.
+    # The_X_Men.add(titulo='subXindex', lst_items=[("Iniciar",None),("SetUp",None),("Ejemplos",None)])
+    # The_X_Men.config(titulo='subXindex' , suPadre='Menu Z' , indexInPadre='Sobre XindeX' )    
     # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
     # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     # ■ LLAMO A MYSTYCA PARA VISUALIZAR EL MENU ■■■■■■■■■■■                               
-    retorno = The_X_Men.mystyca( titulo='Menu1', head_datapush  = " XINDEX - OVER-MAIN " , pad_x=5 )
+    retorno = The_X_Men.mystyca( titulo='Menu1', head_datapush  = " XindeX - Over-Main " , pad_x=5 )
+    # ■ RETORNO DE MYSTYCA ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+    print(f"■■■ THE END en main() ■■■ {retorno if retorno else 'Sin retorno'} ")
 
-    # 5- RETORNO DE MYSTYCA ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    print(f"::: T H E   E N D  en MAIN() ::: {retorno if retorno else 'no retorno'} ")
-
-
-# ████████████████████████████████████████████ FUNCIONES DEFINIDAS EN XINDEX ███████████████████████████████████████████████
-# ████████████████████████████████████████████ FUNCIONES DEFINIDAS EN XINDEX ███████████████████████████████████████████████
-
-
-
-# ████████████████████████████████████████████ INICIO ███████████████████████████████████████████████
-# ████████████████████████████████████████████ INICIO ███████████████████████████████████████████████
-
+# █■████████████████████--- INICIO ---████████████████████■█
+# █■████████████████████--- INICIO ---████████████████████■█
 if __name__ == "__main__":
     multiprocessing.freeze_support()    # PARA QUE FUNCIONE CORRECTAMENTE EN WINDOWS LA CREACION DE PROCESOS HIJOS (BACKGROUNDS O DEMONIOS)    
     os.system('cls')                    # ---- Limpio la terminal 
